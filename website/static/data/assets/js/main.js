aa15380dde6095fa5f5f7409e6e385452c3b4fd2
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 11:05:32 -0700

    yep

--- website/static/data/assets/js/main.js
+++ website/static/data/assets/js/main.js
@@ -3,26 +3,6 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
-function getCookie(name) {
-    // Split cookie string and get all individual name=value pairs in an array
-    var cookieArr = document.cookie.split(";");
-    
-    // Loop through the array elements
-    for(var i = 0; i < cookieArr.length; i++) {
-        var cookiePair = cookieArr[i].split("=");
-        
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
-        if(name == cookiePair[0].trim()) {
-            // Decode the cookie value and return
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    // Return null if not found
-    return "No Guild Selected";
-}
-
 (function($) {
 
 	var $window = $(window),
