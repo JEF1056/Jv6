34d96e2983596b7f49e4495c9740d97366c73207
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 13:51:34 -0700

    e

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -1,5 +1,3 @@
 function setGuild(guild_settings) {
-    for (var key in guild_settings) {
-        document.getElementById(key).innerHTML = guild_settings[key];
-    }
+    for (var key in guild_settings) {document.getElementById(key).innerHTML = guild_settings[key];}
 }   
