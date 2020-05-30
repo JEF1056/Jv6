1112e8d9e22d278fb822aacc599ab6356893b963
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 14:31:49 -0700

    Update get_vars.js

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -1,10 +1,10 @@
-function setGuild(guild_settings) {
-    document.getElementById("temperature").innerHTML = guild_settings["temperature"];
-    document.getElementById("top_k").innerHTML = guild_settings["top_k"];
-    document.getElementById("top_p").innerHTML = guild_settings["top_p"];
-    document.getElementById("seed").innerHTML = guild_settings["seed"];
-    document.getElementById("auto_seed").innerHTML = guild_settings["auto_seed"];
-    document.getElementById("max_history").innerHTML = guild_settings["max_history"];
-    document.getElementById("max_length").innerHTML = guild_settings["max_length"];
-    document.getElementById("no_sample").innerHTML = guild_settings["no_sample"];
+function setGuild(guild_settings[1]) {
+    document.getElementById("temperature").innerHTML = guild_settings[1]["temperature"];
+    document.getElementById("top_k").innerHTML = guild_settings[1]["top_k"];
+    document.getElementById("top_p").innerHTML = guild_settings[1]["top_p"];
+    document.getElementById("seed").innerHTML = guild_settings[1]["seed"];
+    document.getElementById("auto_seed").innerHTML = guild_settings[1]["auto_seed"];
+    document.getElementById("max_history").innerHTML = guild_settings[1]["max_history"];
+    document.getElementById("max_length").innerHTML = guild_settings[1]["max_length"];
+    document.getElementById("no_sample").innerHTML = guild_settings[1]["no_sample"];
 }
