fe45c687cb0ac54eef6d95e711f81cc48567ec4a
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 14:26:56 -0700

    finally?

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -1,3 +1,10 @@
 function setGuild(guild_settings) {
-    for (var key in guild_settings) {document.getElementById(key).innerHTML = guild_settings[key];}
-}   
+    document.getElementById("temperature").innerHTML = guild_settings["temperature"];
+    document.getElementById("top_k").innerHTML = guild_settings["top_k"];
+    document.getElementById("top_p").innerHTML = guild_settings["top_p"];
+    document.getElementById("seed").innerHTML = guild_settings["seed"];
+    document.getElementById("auto_seed").innerHTML = guild_settings["auto_seed"];
+    document.getElementById("max_history").innerHTML = guild_settings["max_history"];
+    document.getElementById("max_length").innerHTML = guild_settings["max_length"];
+    document.getElementById("no_sample").innerHTML = guild_settings["no_sample"];
+}
