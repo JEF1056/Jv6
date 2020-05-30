4948a2e4de3055819ebdeb025f93b869e0060f68
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 14:49:08 -0700

    e

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -1,5 +1,14 @@
-function setGuild(guild_id, guild_name) {
+function setGuild(guild_id, guild_name, guild_settings) {
     console.log(guild_id)
     console.log(guild_name)
+    console.log(guild_settings)
     document.getElementById('Guild Name').innerHTML = guild_name
+    document.getElementById("temperature").innerHTML = guild_settings[guild_id.toString()][1]["temperature"];
+    document.getElementById("top_k").innerHTML = guild_settings[guild_id.toString()][1]["top_k"];
+    document.getElementById("top_p").innerHTML = guild_settings[guild_id.toString()][1]["top_p"];
+    document.getElementById("seed").innerHTML = guild_settings[guild_id.toString()][1]["seed"];
+    document.getElementById("auto_seed").innerHTML = guild_settings[guild_id.toString()][1]["auto_seed"];
+    document.getElementById("max_history").innerHTML = guild_settings[guild_id.toString()][1]["max_history"];
+    document.getElementById("max_length").innerHTML = guild_settings[guild_id.toString()][1]["max_length"];
+    document.getElementById("no_sample").innerHTML = guild_settings[guild_id.toString()][1]["no_sample"];
 }
