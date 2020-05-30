890d3806e5082124047fad46c1ca4ced1db8c519
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-30 15:41:43 -0700

    ree

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -3,12 +3,12 @@ function setGuild(guild_id, guild_name, guild_settings) {
     console.log(guild_name)
     console.log(guild_settings)
     document.getElementById('Guild Name').innerHTML = guild_name
-    document.getElementById("temperature").innerHTML = guild_settings[guild_id[0]][1]["temperature"];
-    document.getElementById("top_k").innerHTML = guild_settings[guild_id[0]][1]["top_k"];
-    document.getElementById("top_p").innerHTML = guild_settings[guild_id[0]][1]["top_p"];
-    document.getElementById("seed").innerHTML = guild_settings[guild_id[0]][1]["seed"];
-    document.getElementById("auto_seed").innerHTML = guild_settings[guild_id[0]][1]["auto_seed"];
-    document.getElementById("max_history").innerHTML = guild_settings[guild_id[0]][1]["max_history"];
-    document.getElementById("max_length").innerHTML = guild_settings[guild_id[0]][1]["max_length"];
-    document.getElementById("no_sample").innerHTML = guild_settings[guild_id[0]][1]["no_sample"];
+    document.getElementById("temperature").innerHTML = guild_settings[guild_id][1]["temperature"];
+    document.getElementById("top_k").innerHTML = guild_settings[guild_id][1]["top_k"];
+    document.getElementById("top_p").innerHTML = guild_settings[guild_id][1]["top_p"];
+    document.getElementById("seed").innerHTML = guild_settings[guild_id][1]["seed"];
+    document.getElementById("auto_seed").innerHTML = guild_settings[guild_id][1]["auto_seed"];
+    document.getElementById("max_history").innerHTML = guild_settings[guild_id][1]["max_history"];
+    document.getElementById("max_length").innerHTML = guild_settings[guild_id][1]["max_length"];
+    document.getElementById("no_sample").innerHTML = guild_settings[guild_id][1]["no_sample"];
 }
--- website/website.py
+++ website/website.py
@@ -45,8 +45,6 @@ def data():
             guild_settings[int(guild.id)]=(pickle.load(open("../hist/"+str(guild.id)+".p", "rb")))
             guild_settings[int(guild.id)][1]=vars(guild_settings[guild.id][1])
     guild_settings=json.dumps(guild_settings)
-    for gu in guilds_data:
-        print(gu.id)
     
     return render_template("data.html", avatar_url=str(user.avatar_url)+"?size=512", name=user.name, guilds=guilds_data, guild_settings=guild_settings)
 
