d538f0dfaf6b7c8a22c328a9715f2d9a3cfcc0c6
Jess Edmund Fan <jessefan2002@gmail.com>
2020-05-31 15:54:33 -0700

    Update website.py

--- website/website.py
+++ website/website.py
@@ -36,7 +36,7 @@ def callback():
 def submit():
     if request.method == 'POST':
         print(request)
-        print(request.fomm)
+        print(request.form)
         inp = request.form['Setting']
         print(inp)
         return "hi"
