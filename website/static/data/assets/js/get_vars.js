ea8c2e2290eedd4f07fdc51a7104a586dc5adc26
Jess Edmund Fan <jessefan2002@gmail.com>
2020-06-03 14:57:17 -0700

    ree

--- website/static/data/assets/js/get_vars.js
+++ website/static/data/assets/js/get_vars.js
@@ -4,16 +4,7 @@ function setGuild(guild_id, guild_name, guild_settings,guild_icon_hash) {
     console.log(guild_settings)
     var img_url = "https://cdn.discordapp.com/icons/"+guild_id+"/"+guild_icon_hash+".png?size=512"
     console.log(img_url)
-    var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var date = new Date(guild_settings[guild_id]["t1"]*1000);
-    var year = date.getFullYear();
-    var month = months_arr[date.getMonth()];
-    var day = date.getDate();
-    var hours = date.getHours();
-    var minutes = "0" + date.getMinutes();
-    var seconds = "0" + date.getSeconds();
-    var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-    document.getElementById("settings_small_text").innerHTML = "Most recent timestamp: "+convdataTime+"<br>Save file version: "+guild_settings[guild_id]["user_version"];
+    document.getElementById("settings_small_text").innerHTML = "Most recent timestamp: "+get_date(guild_settings[guild_id]["t1"]);+"<br>Save file version: "+guild_settings[guild_id]["user_version"];
     document.getElementById('Guild Name').innerHTML = guild_name;
     document.getElementById("Guild Img").src =img_url;
     document.getElementById("temperature").innerHTML = guild_settings[guild_id]["settings"]["temperature"];
@@ -24,6 +15,19 @@ function setGuild(guild_id, guild_name, guild_settings,guild_icon_hash) {
     document.getElementById("max_history").innerHTML = guild_settings[guild_id]["settings"]["max_history"];
     document.getElementById("max_length").innerHTML = guild_settings[guild_id]["settings"]["max_length"];
     document.getElementById("no_sample").innerHTML = guild_settings[guild_id]["settings"]["no_sample"];
+}
+
+function get_date(timest) {
+    var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var date = new Date(timest*1000);
+    var year = date.getFullYear();
+    var month = months_arr[date.getMonth()];
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+    var finaldate = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return finaldate
 }
 
 function send_change(data) {
