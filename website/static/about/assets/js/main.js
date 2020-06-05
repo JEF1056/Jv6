a768228968e3ea6b752e379a0b67324a7153b922
Jess Edmund Fan <jessefan2002@gmail.com>
2020-06-04 20:46:46 -0700

    ok

--- website/static/about/assets/js/main.js
+++ website/static/about/assets/js/main.js
@@ -121,3 +121,21 @@
 		});
 
 })(jQuery);
+
+(function(){
+
+    var parallax = document.querySelectorAll("body"),
+        speed = 0.5;
+  
+    window.onscroll = function(){
+      [].slice.call(parallax).forEach(function(el,i){
+  
+        var windowYOffset = window.pageYOffset,
+            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+  
+        el.style.backgroundPosition = elBackgrounPos;
+  
+      });
+    };
+  
+  })();
