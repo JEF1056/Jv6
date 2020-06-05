4fa6e9cabb303c4d1e9e59953135dbe21e3492e4
Jess Edmund Fan <jessefan2002@gmail.com>
2020-06-04 20:56:52 -0700

    Update main.js

--- website/static/about/assets/js/main.js
+++ website/static/about/assets/js/main.js
@@ -131,9 +131,9 @@
       [].slice.call(parallax).forEach(function(el,i){
   
         var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "100% " + (windowYOffset * speed) + "px";
+            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
   
-        el.style.backgroundPosition = elBackgrounPos;
+        el.style.backgroundPosition = elBackgrounPos-100;
   
       });
     };
