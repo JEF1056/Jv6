bab729169d0062f6be8a0e6d8342d6b2f24a172f
Jess Edmund Fan <jessefan2002@gmail.com>
2020-06-04 20:52:32 -0700

    Update main.js

--- website/static/about/assets/js/main.js
+++ website/static/about/assets/js/main.js
@@ -131,7 +131,7 @@
       [].slice.call(parallax).forEach(function(el,i){
   
         var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+            elBackgrounPos = "100% " + (windowYOffset * speed) + "px";
   
         el.style.backgroundPosition = elBackgrounPos;
   
