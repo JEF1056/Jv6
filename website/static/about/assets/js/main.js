a334cfbad8d4e3aa8d8db9e077842bd94f897a64
Jess Edmund Fan <jessefan2002@gmail.com>
2020-06-04 20:51:25 -0700

    Update main.js

--- website/static/about/assets/js/main.js
+++ website/static/about/assets/js/main.js
@@ -125,13 +125,13 @@
 (function(){
 
     var parallax = document.querySelectorAll("body"),
-        speed = 0.5;
+        speed = -0.5;
   
     window.onscroll = function(){
       [].slice.call(parallax).forEach(function(el,i){
   
         var windowYOffset = window.pageYOffset,
-            elBackgrounPos = "-50% " + (windowYOffset * speed) + "px";
+            elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
   
         el.style.backgroundPosition = elBackgrounPos;
   
